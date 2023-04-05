35780f105f7257e6bfe2263d489faec05a781ea7
Queenzy-Ongeye <quienzy@food4education.org>
2023-04-05 16:00:38 +0300

    feat: added Sql2o dependencies

---
+++ src/main/java/org/dev/database/DBase.java
@@ -0,0 +1,8 @@
+package org.dev.database;
+
+import org.sql2o.Sql2o;
+
+public class DBase {
+
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432", "postgres", "code001");
+}
