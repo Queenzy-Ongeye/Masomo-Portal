97a9d5aca439f9d3117c4ce0df0072b4e3bd1281
Queenzy-Ongeye <quienzy@food4education.org>
2023-04-14 13:10:54 +0300

    feat: add functionality to connect to postgresql

--- src/main/java/org/dev/Main.java
+++ src/main/java/org/dev/Main.java
@@ -1,7 +1,22 @@
 package org.dev;
 
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String connectionString = "jdbc:postgresql://localhost:5432/quiz-app-portal";
+        Sql2o sql2o = new Sql2o(connectionString, "postgres", "code001");
+
+        try(Connection connection = sql2o.open()){
+            String createUserTableSql = "CREATE TABLE user_table ("
+                    + "id SERIAL PRIMARY KEY,"
+                    + "firstName VARCHAR(50) NOT NULL,"
+                    + "lastName VARCHAR(50) NOT NULL,"
+                    + "email VARCHAR(100) NOT NULL,"
+                    + "password VARCHAR(100) NOT NULL"
+                    + ")";
+            connection.createQuery(createUserTableSql).executeUpdate();
+        }
     }
 }
