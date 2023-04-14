9a9a8eff8d87af0c2779f90aa9736fb75ccf5378
Queenzy-Ongeye <quienzy@food4education.org>
2023-04-14 13:09:18 +0300

    feat: added a data base file

--- src/main/java/org/dev/database/DBase.java
+++ src/main/java/org/dev/database/DBase.java
@@ -4,5 +4,5 @@ import org.sql2o.Sql2o;
 
 public class DBase {
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432", "postgres", "code001");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/quiz-app-portal", "postgres", "code001");
 }
